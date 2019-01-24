95913f090c2498c1a18bd465b7084a70d8c72b7c
horse <littlehorseboy@gmail.com>
2019-01-24 09:43:22 +0800

    fix: vscode debug | .babelrc 吃不到 babel.config.js 才吃得到

--- __test__/CheckboxWithLabel.test.js
+++ __test__/CheckboxWithLabel.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-dom/test-utils';
-import Index from '../src/index.jsx';
+import CheckboxWithLabel from '../CheckboxWithLabel.jsx';
 
 it('確定組件 textContent 以及 checkbox click 觸發 change 之後的 textContent', () => {
   const checkbox = TestUtils.renderIntoDocument(
-    <Index labelOn="On" labelOff="Off" />
+    <CheckboxWithLabel labelOn="On" labelOff="Off" />
   );
 
   const checkboxNode = ReactDOM.findDOMNode(checkbox);
